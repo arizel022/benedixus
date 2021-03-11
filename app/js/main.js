8d1a48a0c7c507c209f5d3394bb0148ffba3d347
arizel022 <arizel022@gmail.com>
2021-03-11 19:53:18 +0100

    add main-page

---
+++ app/js/main.js
@@ -0,0 +1,17 @@
+$('.hero').on('click','a', function (event) {
+    event.preventDefault();
+    var id  = $(this).attr('href'),
+        top = $(id).offset().top;
+    $('body,html').animate({scrollTop: top}, 1200);
+});
+$('.service').on('click','a', function (event) {
+    event.preventDefault();
+    var id  = $(this).attr('href'),
+        top = $(id).offset().top;
+    $('body,html').animate({scrollTop: top}, 1200);
+});
+
+
+
+
+
